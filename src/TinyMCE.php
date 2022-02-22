d13e0ae2e7ba6cfbfeb2e6f019ab89e9cc6161b5
enjoys <deadly.info@gmail.com>
2022-02-23 00:02:58 +0300

    symlink langs

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -25,6 +25,7 @@ class TinyMCE implements WysiwygInterface
     {
         $path = str_replace($_ENV['PROJECT_DIR'], '', realpath(__DIR__.'/../'));
         Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/../node_modules/tinymce');
+        Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce/langs', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/langs');
         Assets::js(__DIR__ . '/../node_modules/tinymce/tinymce.min.js');
     }
 }
