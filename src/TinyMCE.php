a611ffea56a5a0a597a803bd80da79ae1b1c9ba8
enjoys <deadly.info@gmail.com>
2022-02-22 23:44:06 +0300

    fix symlink

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -23,8 +23,9 @@ class TinyMCE implements WysiwygInterface
 
     private function initialize()
     {
-        Assets::createSymlink('assets/WYSIWYG/tinymce/node_modules/tinymce', __DIR__ . '/../node_modules/tinymce');
-        Assets::createSymlink('assets/WYSIWYG/tinymce/node_modules/tinymce/langs/ru.js', __DIR__ . '/langs/ru.js');
+        $path = str_replace($_ENV['PROJECT_DIR'], '', realpath(__DIR__.'/../'));
+        Assets::createSymlink(sprintf('%s/assets%s/tinymce/node_modules/tinymce', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/../node_modules/tinymce');
+        Assets::createSymlink(sprintf('%s/assets%s/tinymce/node_modules/tinymce/langs/ru.js', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/langs/ru.js');
         Assets::js(__DIR__ . '/../node_modules/tinymce/tinymce.min.js');
     }
 }
