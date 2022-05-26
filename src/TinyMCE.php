3c056429902696a1a1b04041a9f1c53f027c943f
Enjoys <deadly.info@gmail.com>
2022-05-26 11:32:34 +0300

    actualize code for new interface

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -8,24 +8,30 @@ use EnjoysCMS\Core\Components\WYSIWYG\WysiwygInterface;
 
 class TinyMCE implements WysiwygInterface
 {
-    private string $twigTemplate;
+    private ?string $twigTemplate = null;
 
-    public function __construct(string $twigTemplate = null)
+    public function __construct()
     {
-        $this->twigTemplate = $twigTemplate ?? '@wysisyg/tinymce/src/template/basic.tpl';
         $this->initialize();
     }
 
-    public function getTwigTemplate()
+    public function setTwigTemplate(?string $twigTemplate): void
     {
-        return $this->twigTemplate;
+        $this->twigTemplate = $twigTemplate;
+    }
+
+    public function getTwigTemplate(): string
+    {
+        return $this->twigTemplate  ?? '@wysisyg/tinymce/src/template/basic.tpl';
     }
 
     private function initialize()
     {
-        $path = str_replace($_ENV['PROJECT_DIR'], '', realpath(__DIR__.'/../'));
+        $path = str_replace(realpath($_ENV['PROJECT_DIR']), '', realpath(__DIR__.'/../'));
         Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/../node_modules/tinymce');
         Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce/langs', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/langs');
         Assets::js(__DIR__ . '/../node_modules/tinymce/tinymce.min.js');
     }
+
+
 }
