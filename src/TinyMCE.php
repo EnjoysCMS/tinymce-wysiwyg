d5cb94f1231a4eca666c711d155408f9506ad0a4
Enjoys <deadly.info@gmail.com>
2022-08-22 16:07:36 +0300

    replace $_ENV['PROJECT_DIR'] on getenv('ROOT_PATH') enjoyscms/core ^4.5

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -27,7 +27,7 @@ class TinyMCE implements WysiwygInterface
 
     private function initialize()
     {
-        $path = str_replace(realpath($_ENV['PROJECT_DIR']), '', realpath(__DIR__.'/../'));
+        $path = str_replace(getenv('ROOT_PATH'), '', realpath(__DIR__.'/../'));
         Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/../node_modules/tinymce');
         Assets::createSymlink(sprintf('%s/assets%s/node_modules/tinymce/langs', $_ENV['PUBLIC_DIR'], $path), __DIR__ . '/langs');
         Assets::js(__DIR__ . '/../node_modules/tinymce/tinymce.min.js');
